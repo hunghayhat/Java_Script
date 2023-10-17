dec189cfc2e3aa4243dac8b6a633705954beaf46
HUNG@LAPTOP-7APO7215 <you@example.comHUNG@LAPTOP-7APO7215>
2023-10-17 08:22:35 +0700

    up

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("Hello world!");
+let message = "Hello world";
+console.log(message);
 console.log("I'm learning JS");
+
+console.log(1);
+console.log(2);
+console.log(3);
+
+console.log("John Doe");
+console.log(19);
+console.log("Student");
